5177e49539b88a443c6bd67bfad7115056ef0a24
Serhii Chernenko <28815318+serhii-chernenko@users.noreply.github.com>
2026-02-09 19:20:53 +0100

    latest resend version 6.9.1 installed that causes error

--- server/api/hello.ts
+++ server/api/hello.ts
@@ -1,5 +1,8 @@
 import { defineHandler } from "nitro/h3";
+import { Resend } from 'resend'
 
 export default defineHandler((event) => {
+  const resend = new Resend('api_key')
+
   return { api: "works!"}
 });
